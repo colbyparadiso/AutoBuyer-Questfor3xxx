af2a8c830634ed48f98e61035cbae0ccbf882970
colbyparadiso <colbyparadiso@gmail.com>
2021-03-15 13:01:52 -0400

    TODO's and organization.

--- app/gpuautobuy/src/main/java/com/gpu/autobuy/App.java
+++ app/gpuautobuy/src/main/java/com/gpu/autobuy/App.java
@@ -7,5 +7,44 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
+
+        //TODO:
+        //Selected Options, Listen to Controller, Set Config.
+
+        //TODO:
+        //Cycle Pings on sites for supply.
+        //TODO:
+        //TEMP TEST RUN, Single Site ping and email.
+        //-----------------------------------------------------------
+        
+
+
+        //-----------------------------------------------------------
+
+        //TODO:
+        //If ping, notify.
+
+        //TODO:
+        //If options allow, autobuy.
+
+
+
     }
 }
+
+/* TODO:
+public class Site {
+    public:
+        string url;
+        //APIObjectPull = Site specific API call;
+        //APIObjectPush = Site specific API call;
+        //etc.
+
+    private:
+        string acc;
+        string pass;
+        //Necessery API calls for remaining logged in.
+
+    
+}
+*/
